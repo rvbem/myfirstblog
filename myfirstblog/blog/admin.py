fb99e66a78ab992f7823f6db6288d9638acf5ecb
rvbem@yandex.ru <rvbem@yandex.ru>
2016-12-02 11:30:41 +0300

    Сделал 2е задание есть не понимаю как выполнить 7 пункт задания с добавлением и редактированием записей

--- myfirstblog/blog/admin.py
+++ myfirstblog/blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from blog.models import *
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("category", "author", "title")
+    search_fields = ("category", "author")
+    date_hierarchy = 'creation_date'
+    list_filter = ("author", "category",)
+    list_editable = ("author", "title")
+
 admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 
 # Register your models here.
